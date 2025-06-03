1fde8fafdd971de69a21adea503fbf0a61032c82
Jonathan W. <150624002+marte2050@users.noreply.github.com>
2025-06-02 21:38:35 -0300

    :bug: fix: Update ControllerHelloWorld to use ProxyServiceHelloWorld instead of HelloWorldService

--- pattern-design-proxy/controller/controller_hello_world.py
+++ pattern-design-proxy/controller/controller_hello_world.py
@@ -1,8 +1,8 @@
-from services import HelloWorldService
+from proxy import ProxyServiceHelloWorld
 
 class ControllerHelloWorld:
     def __init__(self) -> None:
-        self.hello_world_service = HelloWorldService()
+        self.hello_world_service = ProxyServiceHelloWorld()
 
     async def get_message(self, id: str) -> dict:
         return await self.hello_world_service.get_message(id)
